f69a3933fad2b757c87298bc77a3eb4c72a1e92b
Tharun Borra <tharun@intverse.io>
2023-03-13 21:10:06 -0400

    Intital Kafka Producer code commit

---
+++ src/main/java/com/intverse/producer/controller/ProducerController.java
@@ -0,0 +1,25 @@
+package com.intverse.producer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping()
+@RestController
+public class ProducerController {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public ProducerController(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestBody String message) {
+        kafkaTemplate.send("test_topic", message);
+        return "Message sent successfully!";
+    }
+}
